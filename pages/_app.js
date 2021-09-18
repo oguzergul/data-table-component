a37c6860bd9c94194489d84e4236b277e4f54628
oguzergul <erguloguz@hotmail.com>
2021-09-19 00:00:41 +0300

    Add: Necessary npm packages has been initialized.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import GlobalStyle from '../styles/global';
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+
+    return <>
+        <Head>
+            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700;800&display=swap"
+                  rel="stylesheet"/>
+        </Head>
+        <GlobalStyle/>
+        <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
