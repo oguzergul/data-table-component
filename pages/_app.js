a05c7c110e14897420d9f9dd2e2878c7ef98d298
oguzergul <erguloguz@hotmail.com>
2021-09-19 00:35:52 +0300

    Update: Global font usage refactored.

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,8 @@
 import GlobalStyle from '../styles/global';
-import Head from "next/head";
 
 function MyApp({Component, pageProps}) {
 
     return <>
-        <Head>
-            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700;800&display=swap"
-                  rel="stylesheet"/>
-        </Head>
         <GlobalStyle/>
         <Component {...pageProps} />
     </>
