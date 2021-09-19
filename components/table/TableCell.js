85f7b9a52589da48ddf81d797798ac2f51c47448
oguzergul <erguloguz@hotmail.com>
2021-09-19 05:22:05 +0300

    Update: Table Cells made liquid. It can resize as given width.

--- components/table/TableCell.js
+++ components/table/TableCell.js
@@ -2,35 +2,40 @@ import styled from "styled-components";
 import Avatar from "../global/Avatar";
 import CustomText from "../global/CustomText";
 import StatusBadge from "../global/StatusBadge";
-const TableCell = ({image, textOne, textTwo, status}) => {
-    const TableCell = styled.td`
-      display: ${image ? 'flex' : ''};
-      align-items: ${image ? 'center' : ''};
-      border: none;
-      border-bottom: 1px solid #E5E7EB;
 
-      & .cell-inner {
-        margin-left: ${image ? '1rem' : 0};
-      }
 
-      :first-child {
-        position: sticky;
-        left: 0;
-        z-index: 1;
-        background: #ffffff;
-      }
-    `;
+const TableCells = styled.td`
+  display: ${props => props.hasImage ? 'flex' : ''};
+  align-items: ${props => props.hasImage ? 'center' : ''};
+  border: none;
+  border-bottom: 1px solid #E5E7EB;
+  width: ${props => props.width || ""};
+
+  & .cell-inner {
+    margin-left: ${props => props.hasImage ? '1rem' : 0};
+  }
+
+  :first-child {
+    position: sticky;
+    left: 0;
+    z-index: 1;
+    background: #ffffff;
+  }
+`;
+
+
+const TableCell = ({image, textOne, textTwo, status,width}) => {
     return (
-        <TableCell>
+        <TableCells width={width} hasImage={!!image}>
             {image ? <Avatar src={image}/> : ''}
             {textTwo ?
                 <div className='cell-inner'>
-                    <CustomText primary={true} text={textOne}/>
+                    <CustomText type={'primary'} text={textOne}/>
                     <CustomText text={textTwo}/>
                 </div> : ''}
             {status ? <StatusBadge status={status}/> : ''}
 
-        </TableCell>
+        </TableCells>
     )
 }
 export default TableCell;
